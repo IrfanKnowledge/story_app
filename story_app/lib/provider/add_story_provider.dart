c46742a953849a9e52ebf30b8cd0a20a802124e2
Irfan <irfanakname.im@gmail.com>
2023-10-09 17:49:56 +0700

    AddStoryPage
    -AddStoryPage, Camera dan Gallery sudah berfungsi dengan baik.
    -AddStoryProvider,
    -Bertambah depencency yaitu image_picker untuk keperluan Media (Misal: Camera dan Gallery).
    -Perizinan pada IOS, kemudian pada Android terdapat perubahan minSdkVersion menjadi 21.
    -Belum bisa upload
    -Mungkin masih perlu rapih2.

---
+++ story_app/lib/provider/add_story_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddStoryProvider extends ChangeNotifier {
+  String? _imagePath;
+
+  XFile? _imageFile;
+
+  String? get imagePath => _imagePath;
+
+  XFile? get imageFile => _imageFile;
+
+  void setImageFile(XFile? value) {
+    _imageFile = value;
+    notifyListeners();
+  }
+
+  void setImagePath(String value) {
+    _imagePath = value;
+    notifyListeners();
+  }
+}
--- story_app/lib/ui/add_story_page.dart
+++ story_app/lib/ui/add_story_page.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:story_app/provider/add_story_provider.dart';
 
 class AddStoryPage extends StatefulWidget {
   const AddStoryPage({super.key});
@@ -17,67 +23,140 @@ class _AddStoryPageState extends State<AddStoryPage> {
         title: const Text('New Story'),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.topCenter,
-          padding: const EdgeInsets.only(
-            top: 32,
-            bottom: 8,
-            right: 16.0,
-            left: 16.0,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
+        child: _buildChangeNotifier(),
+      ),
+    );
+  }
+
+  Widget _buildChangeNotifier() {
+    return ChangeNotifierProvider(
+      create: (_) => AddStoryProvider(),
+      builder: (context, _) => _buildContainer(context),
+    );
+  }
+
+  Container _buildContainer(BuildContext context) {
+    return Container(
+      alignment: Alignment.topCenter,
+      padding: const EdgeInsets.only(
+        top: 32,
+        bottom: 8,
+        right: 16.0,
+        left: 16.0,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          /// context.watch == provider.of<>(context)
+          /// using context.watch so we only listen to 1 state changes,
+          /// that is imagePath only,
+          context.watch<AddStoryProvider>().imagePath == null
+
+              /// if null, show default image
+              ? const Icon(
+                  Icons.image,
+                  size: 100,
+                )
+
+              /// if not null, show image
+              : _showImage(context),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.network(
-                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                height: 250,
-                width: 250,
-                cacheWidth: 512,
-                cacheHeight: 512,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Camera'),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Gallery'),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: _controllerDescription,
-                keyboardType: TextInputType.multiline,
-                maxLines: 5,
-                decoration: const InputDecoration(
-                  hintText: 'Deskripsi',
-                  border: OutlineInputBorder(),
-                  filled: true,
-                ),
-              ),
-              const SizedBox(height: 10),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(
-                    double.infinity,
-                    40,
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Upload'),
+                onPressed: () => _onCameraView(context),
+                child: const Text('Camera'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () => _onGalleryView(context),
+                child: const Text('Gallery'),
               ),
             ],
           ),
-        ),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _controllerDescription,
+            keyboardType: TextInputType.multiline,
+            maxLines: 5,
+            decoration: const InputDecoration(
+              hintText: 'Deskripsi',
+              border: OutlineInputBorder(),
+              filled: true,
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(
+                double.infinity,
+                40,
+              ),
+            ),
+            onPressed: () {},
+            child: const Text('Upload'),
+          ),
+        ],
       ),
     );
+  }
+
+  void _onGalleryView(BuildContext context) async {
+    /// context.read == Provider.of<>(context, listen: false),
+    /// access AddStoryProvider();
+    final provider = context.read<AddStoryProvider>();
+
+    final ImagePicker picker = ImagePicker();
+
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  void _onCameraView(BuildContext context) async {
+    /// context.read == Provider.of<>(context, listen: false),
+    /// access AddStoryProvider();
+    final provider = context.read<AddStoryProvider>();
+
+    final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+    final isIos = defaultTargetPlatform == TargetPlatform.iOS;
+    final isNotMobile = !(isAndroid || isIos);
+    if (isNotMobile) return;
+
+    final ImagePicker picker = ImagePicker();
+
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  Widget _showImage(BuildContext context) {
+    /// context.read == Provider.of<>(context, listen: false),
+    /// access AddStoryProvider();
+    final imagePath = context.read<AddStoryProvider>().imagePath;
+
+    /// check is app running on Web Environment or not
+    return kIsWeb
+        ? Image.network(
+            imagePath.toString(),
+            fit: BoxFit.contain,
+          )
+        : Image.file(
+            File(imagePath.toString()),
+            fit: BoxFit.contain,
+          );
   }
 }
